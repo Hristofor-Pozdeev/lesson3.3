bbe6ea45dfef303ab4d71d15f2446ba13eec0d93
Hristofor-Pozdeev <bordulyakvv@gmail.com>
2024-05-28 14:29:45 +0300

    add new colors in cycle code

--- main.py
+++ main.py
@@ -22,16 +22,24 @@ color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 
 running = True
+is_hovered = False
 while running:
-    screen.fill(color)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEMOTION:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
-                target_x = random.randint(0, SCREEN_WIDTH - target_width)
-                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                is_hovered = True
+            else:
+                is_hovered = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN and is_hovered:
+            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            target_x = random.randint(0, SCREEN_WIDTH - target_width)
+            target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+    screen.fill(color)
     screen.blit(target_img, (target_x, target_y))
     pygame.display.update()
 
